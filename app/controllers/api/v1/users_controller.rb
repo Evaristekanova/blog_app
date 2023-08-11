84ddd82b108b47c0ff1715ff2c21d2aeb588fd5a
Evaristekanova <dusingizimanaevariste3@gmail.com>
2023-08-12 01:01:32 +0200

    add user model and its controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,10 @@ class Api::V1::UsersController < ApplicationController
     render json: @users, status: :ok
   end
 
+  def show
+    render json: @user, status: :ok
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,6 +18,20 @@ class Api::V1::UsersController < ApplicationController
       render json: { errors: @user.errors.full_messages },
              status: :unprocessable_entity
     end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages },
+              status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
   end
 
   private
